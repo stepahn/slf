310533453e81b42fdfeb3cd9ef1f07d4814daa2a
Stephan Zalewski <stephan@foo-labs.com>
2010-09-15 16:58:50 +0200

    added some error handling

--- slf.py
+++ slf.py
@@ -1,29 +1,47 @@
 #/usr/bin/env python
 
-from slflib.simpleparser import parse
+from slflib.simpleparser import parse, ParseError
 from slflib.interpreter import Interpreter
 
 from pypy.rlib.streamio import open_file_as_stream
+import sys
 
 def target(*args):
     return main, None
 
 def main(args):
+    fname = ''
     try:
         fname = args[1]
     except:
-        print 'usage: ', args[0], ' file.slf'
-        sys.exit(1)
+        print 'usage: %s file.slf' % args[0]
+        return 1
 
     interpreter = Interpreter()
     w_module = interpreter.make_module()
 
-    code = open_file_as_stream(fname, 'r', 1024).readall()
-    ast = parse(code)
-    interpreter.eval(ast, w_module)
+    code = ''
+    try:
+        code = open_file_as_stream(fname, 'r', 1024).readall()
+    except Exception, e:
+        print 'ERROR: reading file'
+        print e
+        return 1
+
+    ast = ''
+    try:
+        ast = parse(code)
+    except Exception, e:
+        print 'ERROR: parser', e
+        return 1
+
+    try:
+        interpreter.eval(ast, w_module)
+    except Exception, e:
+        print 'ERROR: interpreter', e
+        return 1
 
     return 0
 
 if __name__ == '__main__':
-    import sys
     main(sys.argv)
