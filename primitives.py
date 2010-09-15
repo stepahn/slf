4022758b7c07fe269cb88d26556628f0f85b6846
Stephan Zalewski <stephan@foo-labs.com>
2010-09-15 19:05:03 +0200

    can't use getattr() to dispatch primitives

--- primitives.py
+++ primitives.py
@@ -1,50 +1,65 @@
 from objmodel import W_Integer, W_NormalObject, W_Method
-import primitives
 
-registered_primitives = {
-    '$int_add': 1,
-    '$int_sub': 1,
-    '$int_mul': 1,
-    '$int_div': 1,
-    '$int_mod': 1,
-    '$bool_gz': 0,
-    '$bool_nor': 1,
-    '$math_sqrt': 0}
-primitives_by_name = registered_primitives.keys()
-all_primitives_arg_count = registered_primitives.values()
+primitives = {}
+primitives_by_name = []
+all_primitives_arg_count = []
 
-def call(methodname, receiver, argumments, builtins):
-    method = getattr(primitives, methodname[1:])
-    args = map(lambda i: i.getvalue(), [receiver] + argumments )
-    int = W_Integer(method(args))
-    int.builtins = builtins
-    return int
+def register(argscount):
+    def g(f):
+        name = '$'+f.__name__
 
+        primitives_by_name.append(name)
+        all_primitives_arg_count.append(argscount)
+        primitives[name] = f
+
+        return f
+    return g
+
+def call(methodname, receiver, arguments, builtins):
+    method = primitives[methodname]
+    args = []
+
+    for a in [receiver.getvalue()] + arguments:
+        if a:
+            args.append(a.intvalue())
+        else:
+            args.append(0)
+
+    my_int = W_Integer(method(args))
+    my_int.builtins = builtins
+    return my_int
+
+@register(1)
 def bool_nor(args):
     return int(not(args[0] or args[1]))
 
+@register(0)
 def bool_gz(args):
     return int(args[0] > 0)
 
+@register(1)
 def int_add(args):
     return args[0] + args[1]
 
+@register(1)
 def int_sub(args):
     return args[0] - args[1]
 
+@register(1)
 def int_mul(args):
     return args[0] * args[1]
 
+@register(1)
 def int_div(args):
     return args[0] / args[1]
 
+@register(1)
 def int_mod(args):
     return args[0] % args[1]
 
+@register(0)
 def math_sqrt(args):
     import math
     return int(math.sqrt(args[0]))
 
-def debug(args):
-    print "dbg: "+repr(args)
 
